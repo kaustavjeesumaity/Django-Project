3a5343bf0869b1516ab8e4dbf48b1ead24fa3a18
kaustavjeesumaity <kaustavjisumaity@gmail.com>
2022-06-10 20:43:23 +0530

    Updated blog/models.py

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from tkinter import CASCADE
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
